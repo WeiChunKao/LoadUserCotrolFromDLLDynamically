bcfaa9edc54e9ca31dbd08d4809089859ddd0f2b
WeiChunKao <34979078+WeiChunKao@users.noreply.github.com>
2022-04-20 16:37:27 +0800

    Add files via upload

---
+++ UserControlDLL/IUserControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserControlDLL
+{
+    public interface IUserControl
+    {
+        event Action<Exception> OnError;
+        event Action<string> OnLog;
+    }
+}
---
+++ UserControlDLL/UC1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserControlDLL
+{
+    public partial class UC1 : UserControl,IUserControl
+    {
+        public event Action<Exception> OnError;
+        public event Action<string> OnLog;
+        public UC1()
+        {
+            InitializeComponent();
+        }
+        private void UC1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                int a = 11;
+                int b = 1;
+                int c = a / b;
+                OnLog("test");
+            }catch(Exception ex)
+            {
+                OnError(ex);
+            }
+        }
+    }
+}
